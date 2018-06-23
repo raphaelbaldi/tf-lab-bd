27f8a46ae6587605842aca58c0f032edf922883b
Raphael Baldi <raphaelbaldi@gmail.com>
2018-06-22 23:22:33 -0300

    Adicionada query para exibir resultados dos jogos

--- source/queries.sql
+++ source/queries.sql
@@ -16,4 +16,15 @@ ORDER BY ID_JOGADOR;
 
 -- SELECIONA TODOS OS JOGOS
 SELECT * FROM TF_JOGOS
-ORDER BY DATA;
+ORDER BY DATA_JOGO;
+
+-- SELECIONA TODOS OS JOGOS E EXIBE O NOME DOS TIMES PARTICIPANTES, ASSIM COMO O RESULTADO
+SELECT j.id_jogo JOGO, j.data_jogo DATA, mandante.nome MANDANTE, gols_mandante GOLS, visitante.nome VISITANTE, gols_visitante GOLS,
+  CASE
+    WHEN gols_mandante > gols_visitante THEN mandante.nome
+    WHEN gols_mandante < gols_visitante THEN visitante.nome
+    ELSE 'EMPATE' END RESULTADO
+FROM tf_jogos j
+  INNER JOIN tf_times mandante ON j.id_mandante = mandante.id_time
+  INNER JOIN tf_times visitante ON j.id_visitante = visitante.id_time
+ORDER BY j.id_jogo;
